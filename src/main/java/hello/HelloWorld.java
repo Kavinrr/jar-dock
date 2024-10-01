a02037feb6ca06868756b19cbf9890478bf1e4b9
Kavinrr <69468796+Kavinrr@users.noreply.github.com>
2024-10-01 18:11:47 +0530

    Add files via upload

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class HelloWorld {
+    public static String getMessage() {
+        return "Hello, World! HOW ARE YOU :)  This was executed to check E-MAIL notification";
+    }
+}

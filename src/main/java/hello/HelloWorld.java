8459de25d21fcb1bc37f48f3288faf317747c34b
Kavinrr <69468796+Kavinrr@users.noreply.github.com>
2024-10-01 18:23:10 +0530

    Delete src/main directory

--- src/main/java/hello/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class HelloWorld {
-    public static String getMessage() {
-        return "Hello, World! HOW ARE YOU :)  This was executed to check E-MAIL notification";
-    }
-}
